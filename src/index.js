9dbb17e0cc80916783e2704ea5dc5d044dc8dd26
Chaos <rizwannafees13@gmail.com>
2021-04-15 18:11:03 -0400

    Added images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './assets/css/styles.css';
+import logo from './assets/images/logo.png';
+import homeHero from './assets/images/homeHero.jpg';
+import aboutUsBg from './assets/images/aboutUsBackground.jpg';
+import aboutUsImg1 from './assets/images/aboutUsImg1.jpg';
+import aboutUsImg2 from './assets/images/aboutUsImg2.jpg';
+import specialMenu from './assets/images/specialMenu.jpg';
+import burger from './assets/images/burger.jpeg';
+import pizza from './assets/images/pizza.jpg';
+import wings from './assets/images/wings.jpg'
+import tender from './assets/images/tenders.jpg'
+import nuggets from './assets/images/nuggets.jpg'
