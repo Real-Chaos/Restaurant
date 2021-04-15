5da4923d5ae18a17dbd939734304cf051a8999a0
Chaos <rizwannafees13@gmail.com>
2021-04-14 21:02:12 -0400

    makig live

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-import sayHello from './ok'
-sayHello()
+const path = require('path');
+import './assets/css/styles.css';
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+ module: {
+   rules: [
+     {
+       test: /\.css$/i,
+       use: ['style-loader', 'css-loader'],
+     },
+   ],
+ },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,17 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+ module: {
+   rules: [
+     {
+       test: /\.css$/i,
+       use: ['style-loader', 'css-loader'],
+     },
+   ],
+ },
 };
-
-module.exports = {
-    entry: {
-      'bundle.min.css': [
-        __dirname + '/assets/css/styles.css',
-        __dirname + '/src/styles/xyz.css',
-        __dirname + '/src/styles/mno.css'
-      ]
-    },
-    devtool: '',
-    output: {
-      path: __dirname + '/dist/styles/',
-      filename: '[name]'
-  
-    },
-    module: {
-      rules: [{
-          test: /\.css$/i,
-          use: extractCSS.extract({
-            use: {
-              loader: 'css-loader',
-              options: {
-                minimize: true
-              }
-            }
-          })
-      }]
-    },
-    resolve: {
-      alias: {},
-      modules: [],
-      extensions: ['.css']
-    },
-    plugins: [
-      extractCSS
-    ]
-  };
