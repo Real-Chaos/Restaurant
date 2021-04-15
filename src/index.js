47d8f9f0caa4952aab9bcff3a4a30dee367ad2c0
Chaos <rizwannafees13@gmail.com>
2021-04-15 13:20:57 -0400

    index.html

--- src/index.js
+++ src/index.js
@@ -1,18 +1,2 @@
-const path = require('path');
 import './assets/css/styles.css';
-
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
- module: {
-   rules: [
-     {
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     },
-   ],
- },
-};
+console.log("hi")
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
+
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
  module: {
    rules: [
      {
-       test: /\.css$/i,
+       test: /\.css$/,
        use: ['style-loader', 'css-loader'],
      },
    ],
